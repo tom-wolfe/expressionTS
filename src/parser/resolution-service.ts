1335629e3e45d6d9394183cb62e0c1fd0cd68a9d
Tom Wolfe <trwolfe13@gmail.com>
2017-11-16 14:48:25 +0000

    Add default functions.

--- src/parser/resolution-service.ts
+++ src/parser/resolution-service.ts
@@ -8,9 +8,36 @@ export interface ResolutionService {
   resolveVariable(name: string, context: ResolutionContext): any;
 }
 
+export interface FunctionMap { [key: string]: (...args: any[]) => any };
+
+const DEFAULT_FUNCTIONS: FunctionMap = {
+  abs: Math.abs,
+  pow: Math.pow,
+  floor: Math.floor,
+  ceil: Math.ceil,
+  exp: Math.exp,
+  log: Math.log,
+  log10: Math.log10,
+  max: Math.max,
+  min: Math.min,
+  sign: Math.sign,
+  sqrt: Math.sqrt
+};
+
 export class DefaultResolutionService implements ResolutionService {
-  public variables: { [key: string]: any };
-  public functions: { [key: string]: (...args: any[]) => any };
+  private _mergedFunctions: FunctionMap = DEFAULT_FUNCTIONS;
+  private _functions: FunctionMap = {};
+
+  public variables: { [key: string]: any } = {};
+
+  public get functions(): FunctionMap {
+    return this._functions;
+  }
+
+  public set functions(value: FunctionMap) {
+    this._functions = value;
+    this._mergedFunctions = Object.assign({}, DEFAULT_FUNCTIONS, this._functions);
+  }
 
   public resolveFunction(name: string, context: ResolutionContext): (...args: any[]) => any {
     return this.functions[name];
